51cc91284b114a91bf8cd66270c1b5ff62bee34f
Rajat Kanti Nandi <rajatkantinandi1993@gmail.comi>
2018-11-03 12:55:45 +0530

    restructured the header & changed font

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+class Header extends React.Component {
+	render() {
+		return (
+			<section class="header">
+				<h1 class="title">Quizard</h1>
+				<small class="tagline">swing your knowledge wand!</small>
+			</section>
+		);
+	}
+}
+export default Header;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,22 +1,18 @@
-import React from 'react';
-import '../stylesheets/Home.css';
+import React from "react";
+import "../stylesheets/Home.css";
+import Header from "./Header";
 
 class Home extends React.Component {
-
 	render() {
 		return (
 			<div>
-				<section class="header">
-					<h1 class="title">Quizard</h1>
-					<small class="tagline">swing your knowledge wand!</small>
-				</section>
+				<Header />
 				<section class="content">
 					<strong>Edit the Home Page!</strong>
 				</section>
 			</div>
 		);
 	}
-
 }
 
 export default Home;
