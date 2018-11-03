70975a67f3bbdbab985abf81a820068395cf3f77
Rajat Kanti Nandi <rajatkantinandi1993@gmail.comi>
2018-11-03 16:21:09 +0530

    Created Question component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,15 @@
 import React from "react";
 import "../stylesheets/Home.css";
 import Header from "./Header";
-
+import Form from "./Form";
+import Question from "./Question";
 class Home extends React.Component {
 	render() {
 		return (
 			<div>
 				<Header />
 				<section class="content">
-					<strong>Edit the Home Page!</strong>
+					<Form />
 				</section>
 			</div>
 		);
